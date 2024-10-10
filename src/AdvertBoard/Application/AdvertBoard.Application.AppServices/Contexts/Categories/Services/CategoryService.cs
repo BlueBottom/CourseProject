dcddffe68a0ee99b235b0276bb56eb80b203a158
Tatyana Lutsishin <xxdelfinchikxx@gmail.com>
2024-10-10 14:02:23 +0300

    Added caching to CategoryService

--- src/AdvertBoard/Application/AdvertBoard.Application.AppServices/Contexts/Categories/Services/CategoryService.cs
+++ src/AdvertBoard/Application/AdvertBoard.Application.AppServices/Contexts/Categories/Services/CategoryService.cs
@@ -1,19 +1,25 @@
 ﻿using AdvertBoard.Application.AppServices.Contexts.Categories.Repositories;
+using AdvertBoard.Application.AppServices.Helpers;
 using AdvertBoard.Application.AppServices.Validators;
 using AdvertBoard.Contracts.Contexts.Categories.Requests;
 using AdvertBoard.Contracts.Contexts.Categories.Responses;
 using AdvertBoard.Domain.Contexts.Categories;
 using AutoMapper;
 using FluentValidation;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace AdvertBoard.Application.AppServices.Contexts.Categories.Services;
 
 /// <inheritdoc/>
 public class CategoryService : ICategoryService
 {
+    private const string RedisKeyCategoryPrefix = "Category:";
+    private const string ParentsCategoriesKey = "parent_categories";
+
     private readonly ICategoryRepository _categoryRepository;
     private readonly IMapper _mapper;
     private readonly BusinessLogicAbstractValidator<CreateCategoryRequest> _createCategoryValidator;
+    private readonly IDistributedCache _distributedCache;
 
     /// <summary>
     /// Инициализирует экземпляр класса <see cref="CategoryService"/>.
@@ -21,41 +27,68 @@ public class CategoryService : ICategoryService
     /// <param name="categoryRepository">Репозиторий для работы с категориями.</param>
     /// <param name="mapper">Маппер.</param>
     /// <param name="createCategoryValidator">Валидатор добавления категории.</param>
+    /// <param name="distributedCache">Распределенный кеш.</param>
     public CategoryService(
-        ICategoryRepository categoryRepository, 
+        ICategoryRepository categoryRepository,
         IMapper mapper,
-        BusinessLogicAbstractValidator<CreateCategoryRequest> createCategoryValidator)
+        BusinessLogicAbstractValidator<CreateCategoryRequest> createCategoryValidator,
+        IDistributedCache distributedCache
+    )
     {
         _categoryRepository = categoryRepository;
         _mapper = mapper;
         _createCategoryValidator = createCategoryValidator;
+        _distributedCache = distributedCache;
     }
 
     /// <inheritdoc/>
     public async Task<Guid> AddAsync(CreateCategoryRequest createCategoryRequest, CancellationToken cancellationToken)
     {
         await _createCategoryValidator.ValidateAndThrowAsync(createCategoryRequest, cancellationToken);
-        
+
         var category = _mapper.Map<CreateCategoryRequest, Category>(createCategoryRequest);
         return await _categoryRepository.AddAsync(category, cancellationToken);
     }
 
     /// <inheritdoc/>
-    public Task<CategoryHierarchyResponse> GetHierarchyByIdAsync(Guid id, CancellationToken cancellationToken)
-    {
-        return _categoryRepository.GetHierarchyByIdAsync(id, cancellationToken);
+    public async Task<CategoryHierarchyResponse> GetHierarchyByIdAsync(Guid id, CancellationToken cancellationToken)
+    {        
+        var key = $"{RedisKeyCategoryPrefix}{id.ToString()}";
+        var categoryFromCache = await _distributedCache.GetByKeyAsync<CategoryHierarchyResponse>(key, cancellationToken);
+        if (categoryFromCache is not null) return categoryFromCache;
+
+        var category = await _categoryRepository.GetHierarchyByIdAsync(id, cancellationToken);
+
+        await _distributedCache.PutByKeyAsync(key, category, 1, cancellationToken);
+        return category;
     }
 
     /// <inheritdoc/>
-    public Task<IEnumerable<ShortCategoryResponse>> GetAllParentsAsync(CancellationToken cancellationToken)
+    public async Task<IEnumerable<ShortCategoryResponse>> GetAllParentsAsync(CancellationToken cancellationToken)
     {
-        return _categoryRepository.GetAllParentsAsync(cancellationToken);
+        var categoriesFromCache = await _distributedCache.GetByKeyAsync<IEnumerable<ShortCategoryResponse>>(
+            ParentsCategoriesKey,
+            cancellationToken);
+        
+        if (categoriesFromCache?.Any() ?? false) return categoriesFromCache;
+        var categories = await _categoryRepository.GetAllParentsAsync(cancellationToken);
+
+        await _distributedCache.PutByKeyAsync(ParentsCategoriesKey, categories, 1, cancellationToken);
+
+        return categories;
     }
 
     /// <inheritdoc/>
-    public Task<ShortCategoryResponse> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    public async Task<ShortCategoryResponse> GetByIdAsync(Guid id, CancellationToken cancellationToken)
     {
-        return _categoryRepository.GetByIdAsync(id, cancellationToken);
+        var key = $"{RedisKeyCategoryPrefix}{id.ToString()}";
+        var categoryFromCache = await _distributedCache.GetByKeyAsync<ShortCategoryResponse>(key, cancellationToken);
+        if (categoryFromCache is not null) return categoryFromCache;
+
+        var category = await _categoryRepository.GetByIdAsync(id, cancellationToken);
+
+        await _distributedCache.PutByKeyAsync(key, category, 1, cancellationToken);
+        return category;
     }
 
     /// <inheritdoc/>
@@ -78,8 +111,12 @@ public class CategoryService : ICategoryService
     }
 
     /// <inheritdoc/>
-    public Task<bool> IsCategoryExists(Guid id, CancellationToken cancellationToken)
+    public async Task<bool> IsCategoryExists(Guid id, CancellationToken cancellationToken)
     {
-        return _categoryRepository.IsCategoryExists(id, cancellationToken);
+        var key = $"{RedisKeyCategoryPrefix}{id.ToString()}";
+        var categoryFromCache = await _distributedCache.GetByKeyAsync<CategoryHierarchyResponse>(key, cancellationToken);
+        if (categoryFromCache is not null) return true;
+        
+        return await _categoryRepository.IsCategoryExists(id, cancellationToken);
     }
 }
